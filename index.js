e7622df64b86e1f888c1a940c5be162d1ef4442c
Juan Carrera <juankarrera@gmail.com>
2020-01-16 10:10:17 -0500

    add error handling for all paths and http requests using middleware

--- index.js
+++ index.js
@@ -13,4 +13,18 @@ app.get("/about", (req, res, next) => {
   res.send("This is the about route!");
 });
 
+// this matches all routes and all methods
+app.use((req, res, next) => {
+  res.status(404).send({
+    status: 404,
+    error: 'Not found'
+  });
+});
+
+// error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something Broke!');
+});
+
 app.listen(port, () => console.log(`App listening on port: ${port}`));
